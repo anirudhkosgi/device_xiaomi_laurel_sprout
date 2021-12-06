0dccce8d8031ac0a21215ad32409d56153235ee2
itstarzan <rextzan17@gmail.com>
2021-12-06 10:55:42 +0000

    ll

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common CrDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
