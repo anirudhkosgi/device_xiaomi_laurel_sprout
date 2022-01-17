6b02d6bc0f5170994f98491ecf27a7dd83607ac7
anirudhkosgi <kosgi34@gmail.com>
2022-01-17 16:31:36 +0000

    Revert "laurel_sprout: remove XiaomiParts"
    This reverts commit 35780d77015b69df5fea8979bc0fb622efeb293b.

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -270,6 +270,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.xiaomi_parts.rc \
     ueventd.qcom.rc
 
 # IRQ
@@ -515,6 +516,10 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl \
     libwfdaac_vendor
+
+# Xiaomi
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # Inherit the proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
